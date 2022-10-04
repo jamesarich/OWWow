3bab2e33cdde1af51378c737eeebe10c2b869f75
James Rich <2199651+jamesarich@users.noreply.github.com>
2022-10-04 10:55:06 -0500

    UI updates for list and details, initial tests (#2)

--- app/src/main/java/app/mobilemobile/owwow/MainActivity.kt
+++ app/src/main/java/app/mobilemobile/owwow/MainActivity.kt
@@ -1,28 +1,19 @@
 package app.mobilemobile.owwow
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import app.mobilemobile.owwow.data.Video
-import app.mobilemobile.owwow.data.Wow
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import app.mobilemobile.owwow.ui.theme.OWWowTheme
+import app.mobilemobile.owwow.ui.views.WowDetail
+import app.mobilemobile.owwow.ui.views.WowList
 
 class MainActivity : ComponentActivity() {
     private val mainViewModel by viewModels<MainViewModel>()
@@ -30,70 +21,47 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            OWWowTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    val listState = rememberLazyListState()
-                    LazyColumn(state = listState) {
-                        itemsIndexed(mainViewModel.wows) { _, wow ->
-                            WowCard(wow)
-                        }
-                    }
-                }
+            val navController = rememberNavController()
+            MainContent(
+                navController = navController,
+                mainViewModel = mainViewModel,
+                siteButtonClicked = { siteButtonClicked() })
+        }
+    }
+
+    private fun siteButtonClicked() {
+        startActivity(
+            Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://owen-wilson-wow-api.herokuapp.com")
+            )
+        )
+    }
+
+}
+
+@Composable
+fun MainContent(
+    navController: NavHostController,
+    mainViewModel: MainViewModel,
+    siteButtonClicked: () -> Unit
+) {
+    OWWowTheme {
+        NavHost(navController = navController, startDestination = "wowList") {
+            composable("wowList") {
+                WowList(
+                    wows = mainViewModel.wows,
+                    onWowClicked = { wow ->
+                        mainViewModel.onWowClicked(wow)
+                        navController.navigate("wowDetail")
+                    },
+                    onSiteButtonClicked = { siteButtonClicked() },
+                )
+            }
+            composable("wowDetail") {
+                WowDetail(wow = mainViewModel.clickedWow)
             }
         }
     }
 }
 
-@Suppress("FunctionNaming")
-@Composable
-fun WowCard(wow: Wow) {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
-            .padding(8.dp)
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentHeight()
-                .padding(8.dp)
-        ) {
-            Text(text = "${wow.movie} - Wow ${wow.currentWowInMovie} of ${wow.totalWowsInMovie}")
-        }
-    }
-}
-
-@Suppress("FunctionNaming")
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    val testVideo = Video(
-        quality1080p = "1080",
-        quality720p = "760,",
-        quality480p = "480",
-        quality360p = "360"
-    )
-    val testWow = Wow(
-        movie = "Wow Movie",
-        year = 2023,
-        releaseDate = "march 1",
-        director = "Wow Director",
-        character = "Wow Character",
-        movieDuration = "120mins",
-        timestamp = "timestamp",
-        fullLine = "wow",
-        currentWowInMovie = 1,
-        totalWowsInMovie = 3,
-        poster = "poster",
-        video = testVideo,
-        audio = "audio"
-    )
-    OWWowTheme {
-        WowCard(testWow)
-    }
-}
---
+++ app/src/main/java/app/mobilemobile/owwow/ui/views/WowDetail.kt
@@ -0,0 +1,128 @@
+package app.mobilemobile.owwow.ui.views
+
+import android.net.Uri
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.media3.common.C
+import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
+import androidx.media3.datasource.DataSource
+import androidx.media3.datasource.DefaultDataSource
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.source.ProgressiveMediaSource
+import androidx.media3.ui.PlayerView
+import app.mobilemobile.owwow.data.Wow
+import app.mobilemobile.owwow.ui.theme.OWWowTheme
+import com.skydoves.landscapist.ImageOptions
+import com.skydoves.landscapist.glide.GlideImage
+
+
+@Suppress("FunctionNaming")
+@Composable
+fun WowDetail(wow: Wow) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "WOW!", fontSize = 96.sp)
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .padding(8.dp)
+            ) {
+                Column {
+                    VideoPlayer(uri = Uri.parse(wow.video.quality480p))
+                    Row(
+                        modifier = Modifier.padding(8.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        GlideImage(
+                            imageModel = wow.poster,
+                            imageOptions = ImageOptions(contentScale = ContentScale.Fit),
+                            modifier = Modifier.size(128.dp)
+                        )
+                        Column(modifier = Modifier.padding(8.dp)) {
+                            Text(text = "Movie: ${wow.movie}")
+                            Text(text = "Character: ${wow.character}")
+                            Text(text = "Occurrence in movie: ${wow.currentWowInMovie} of ${wow.totalWowsInMovie}")
+                            Text(text = "Full Line: ${wow.fullLine}")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+@androidx.annotation.OptIn(androidx.media3.common.util.UnstableApi::class)
+fun VideoPlayer(uri: Uri) {
+    val context = LocalContext.current
+
+    val exoPlayer = remember {
+        ExoPlayer.Builder(context)
+            .build()
+            .apply {
+                val defaultDataSourceFactory = DefaultDataSource.Factory(context)
+                val dataSourceFactory: DataSource.Factory = DefaultDataSource.Factory(
+                    context,
+                    defaultDataSourceFactory
+                )
+                val source = ProgressiveMediaSource.Factory(dataSourceFactory)
+                    .createMediaSource(MediaItem.fromUri(uri))
+
+                setMediaSource(source)
+                prepare()
+            }
+    }
+    exoPlayer.playWhenReady = true
+    exoPlayer.videoScalingMode = C.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING
+    exoPlayer.repeatMode = Player.REPEAT_MODE_ONE
+
+    DisposableEffect(
+        AndroidView(factory = {
+            PlayerView(context).apply {
+                hideController()
+                useController = false
+                player = exoPlayer
+            }
+        })
+    ) {
+        onDispose { exoPlayer.release() }
+    }
+}
+
+
+@Preview
+@Composable
+fun WowDetailPreview() {
+    OWWowTheme {
+        WowDetail(wow = testWow)
+    }
+}
